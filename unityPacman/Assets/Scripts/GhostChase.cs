6302b9d8c0110084b98a2d857b3fd1824f9d258d
boris.shulyak <55171497+BorysShulyak@users.noreply.github.com>
2021-11-21 13:09:08 +0200

    Initial set up GhostBehavior.

--- unityPacman/Assets/Scripts/GhostChase.cs
+++ unityPacman/Assets/Scripts/GhostChase.cs
@@ -1,18 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GhostChase : MonoBehaviour
+public class GhostChase : GhostBehavior
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 }
